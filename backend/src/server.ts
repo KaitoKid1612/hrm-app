73dcfebda907a65dd7542855822b272a398ad8d3
vietlv <vietlv@vietis.com.vn>
2025-10-06 16:27:51 +0700

    feat: config base source

---
+++ backend/src/server.ts
@@ -0,0 +1,16 @@
+import express from "express";
+// @ts-ignore
+import cors from "cors";
+import helmet from "helmet";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(helmet());
+app.use(express.json());
+
+app.get("/", (_, res) => res.json({ message: "Backend running 🚀" }));
+
+app.listen(5000, () => console.log("✅ Backend on port 5000"));
